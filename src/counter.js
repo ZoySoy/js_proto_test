984adc893d80947ebe4b555b2809533c6a013588
anton_zubov <anton_zubov@outlook.com>
2020-01-20 23:37:58 +0300

    added some comments;

--- src/counter.js
+++ src/counter.js
@@ -1,15 +1,18 @@
 module.exports = Counter;
 
+// constructor function
 function Counter(counterValue) {
     this.counterValue = counterValue || 0;
 }
 
+// for each object of type Counter
 Counter.prototype.getCounterValue = function() {
     return this.calculateCounterValue
         ? this.calculateCounterValue()
         : this.counterValue;
 }
 
+// class level function (e.g. static)
 Counter.sum = function(counters) {
     var result = 0;
     counters.forEach(counter => result += counter.getCounterValue());
--- src/entrance.js
+++ src/entrance.js
@@ -6,8 +6,13 @@ function Entrance(floors) {
     this.floors = floors || [];
 }
 
+// set parent
 Entrance.prototype = new Counter();
+
+// set parent constructor (super())
 Entrance.prototype.super = Counter;
+
+// define own constructor (because it was setted with parent's one after we set Counter as a parent)
 Entrance.prototype.constructor = Entrance;
 
 Entrance.prototype.calculateCounterValue = function() {
